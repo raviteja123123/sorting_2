434d3dcd9a46daf37ce8b585a13b22a24755cc3a
raviteja123123 <kavetijeevansai@gmail.com>
2023-11-18 14:35:52 +0530

    updated

--- git-to-jenkins.py
+++ git-to-jenkins.py
@@ -3,3 +3,5 @@ print("I am from git to jenkins pipeline")
 print("I am latest version ")
 #print(f"aws configureation :{os.system("aws configure list")}")
 os.system("aws configure list")
+os.system("aws s3 ls humanetics-bucket-nov-15")
+
