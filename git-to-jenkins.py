b435e2fb43341e758d5ec44bad022b1bca6f6684
raviteja123123 <kavetijeevansai@gmail.com>
2023-11-18 15:17:41 +0530

    updated

--- git-to-jenkins.py
+++ git-to-jenkins.py
@@ -1,7 +1,20 @@
-import os
+import argparse
+#import os
 print("I am from git to jenkins pipeline")
 print("I am latest version ")
+
+
+
+parser = argparse.ArgumentParser(description='Process some strings.')
+
+parser.add_argument('-I',help="please pass the input string")
+parser.add_argument('-O',help="please pass the output string")
+
+args = parser.parse_args()
+
+print(f"{args.I}{args.O}")
+
 #print(f"aws configureation :{os.system("aws configure list")}")
-os.system("aws configure list")
-os.system("aws s3 ls humanetics-bucket-nov-15")
+#os.system("aws configure list")
+#os.system("aws s3 ls humanetics-bucket-nov-15")
 
